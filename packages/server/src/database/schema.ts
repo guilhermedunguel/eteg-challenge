818389a2e0967cc6cdb5b5fb3604b89f0391aa00
Guilherme Dunguel <gdunguel2012@gmail.com>
2026-05-26 20:46:31 -0300

    fix(server): add unique constraint to hex at colors table

--- packages/server/src/database/schema.ts
+++ packages/server/src/database/schema.ts
@@ -3,7 +3,7 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 export const colorsTable = pgTable("colors", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
   name: varchar("name", { length: 50 }).notNull().unique(),
-  hex: varchar("hex", { length: 7 }).notNull(),
+  hex: varchar("hex", { length: 7 }).notNull().unique(),
 });
 
 export const clientsTable = pgTable("clients", {
