7880784444833ec3c78f3595b5e8ac5798084700
Guilherme Dunguel <gdunguel2012@gmail.com>
2026-05-26 21:24:48 -0300

    feat(server): create clients repository

---
+++ packages/server/src/repositories/clients-repository.ts
@@ -0,0 +1,53 @@
+import { eq } from "drizzle-orm";
+import { Database } from "../database";
+import { clientsTable } from "../database/schema";
+
+type ClientDTO = typeof clientsTable.$inferInsert;
+type Client = typeof clientsTable.$inferSelect;
+
+export class ClientsRepository {
+  constructor(private database: Database) {}
+
+  async findAll({ page, limit }: { page: number; limit: number }) {
+    return this.database
+      .select()
+      .from(clientsTable)
+      .limit(limit)
+      .offset((page - 1) * limit);
+  }
+
+  async findById({ id }: Pick<Client, "id">) {
+    const result = await this.database
+      .select()
+      .from(clientsTable)
+      .where(eq(clientsTable.id, id));
+
+    return result;
+  }
+
+  async findByCpf({ cpf }: Pick<Client, "cpf">) {
+    const result = await this.database
+      .select()
+      .from(clientsTable)
+      .where(eq(clientsTable.cpf, cpf));
+
+    return result;
+  }
+
+  async insert(client: ClientDTO) {
+    const result = await this.database
+      .insert(clientsTable)
+      .values(client)
+      .returning({ id: clientsTable.id });
+
+    return result;
+  }
+
+  async remove({ id }: Pick<Client, "id">) {
+    const result = await this.database
+      .delete(clientsTable)
+      .where(eq(clientsTable.id, id));
+
+    return result.rowCount;
+  }
+}
