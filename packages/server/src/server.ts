5c888e846e23dfd664aa551849825a11d9f1d999
Guilherme Dunguel <gdunguel2012@gmail.com>
2026-05-27 14:27:33 -0300

    feat(server): export fastify host

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,6 @@
 import { env } from "../env";
 import { app } from "./app";
 
-app.listen({ port: env.SERVER_PORT }, () => {
+app.listen({ port: env.SERVER_PORT, host: "0.0.0.0" }, () => {
   console.log(`> HTTP Server Running at: http://localhost:${env.SERVER_PORT}/`);
 });
