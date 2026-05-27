de49453d3c16d78f6bd86227724a11bb661e275d
Guilherme Dunguel <gdunguel2012@gmail.com>
2026-05-26 21:07:53 -0300

    feat(server): create colors repository

---
+++ packages/server/src/repositories/colors-repository.ts
@@ -0,0 +1,12 @@
+import { Database } from "../database";
+import { colorsTable } from "../database/schema";
+
+type Color = typeof colorsTable.$inferSelect;
+
+export class ColorsRepository {
+  constructor(private database: Database) {}
+
+  async findAll(): Promise<Color[]> {
+    return this.database.select().from(colorsTable);
+  }
+}
