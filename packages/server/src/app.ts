f86fde0698898de5ac813ddbe6d6fb01935c2297
Guilherme Dunguel <gdunguel2012@gmail.com>
2026-05-27 08:34:07 -0300

    feat(server): create client routes

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,16 +1,50 @@
 import { fastify } from "fastify";
+import { fastifySwagger } from "@fastify/swagger";
+import fastifyScalar from "@scalar/fastify-api-reference";
+
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import { clientsRoutes } from "./routes/clients-routes";
 
-export const app = fastify();
+export const app = fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+});
 
 // Plugins
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-// Routes
-app.get("/", (request, reply) => {
-  reply.status(200).send("OK");
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "ETEG Challenge API",
+      description:
+        "API Criada para o desafio da ETEG, nela é possível cadastrar clientes e atribuir cores.",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
 });
+
+app.register(fastifyScalar, {
+  routePrefix: "/docs",
+});
+
+// Routes
+app.register(
+  async (api) => {
+    api.register(clientsRoutes, { prefix: "/clients" });
+  },
+  { prefix: "/v1" },
+);
